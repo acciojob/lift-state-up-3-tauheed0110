f3d8c6435dbd3e81a3062ede2dc4fb9c0aae0858
saif0110 <khanmohdtauheed4@gmail.com>
2024-11-27 12:41:01 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const hadleSelectedOption = (option)=>{
+    setSelectedOption(option)
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+        <h1>Parent Component</h1>
+        <Child1 hadleSelectedOption={hadleSelectedOption}/>
+        <Child2 hadleSelectedOption={hadleSelectedOption}/>
+        <p>Selected Option: {selectedOption}</p>
     </div>
   )
 }
 
 export default App
+
+
+const Child1 = ({hadleSelectedOption})=>{
+  return (
+    <div>
+      <h1>Child Component</h1>
+      <button onClick={()=>{
+        hadleSelectedOption("Option 1");
+      }}>Option 1</button>
+    </div>
+  )
+}
+const Child2 = ({hadleSelectedOption})=>{
+  return (
+    <div>
+      <h1>Child Component</h1>
+      <button onClick={()=>{
+        hadleSelectedOption("Option 2");
+      }}>Option 2</button>
+    </div>
+  )
+}
